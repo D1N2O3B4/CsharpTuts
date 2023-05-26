e17020ad256f4cdd0f86df4022a974fc5a9ad304
D1N2O3B4 <davebostons2017@gmail.com>
2023-05-26 03:04:54 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace App
+{
+        internal class Tuts
+    {
+        static
+             void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+
+}
